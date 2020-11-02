c83967c16ba5a8f2813fef784a6403f94f9a43ab
BTB0923 <brendan.t.bliss@gmail.com>
2020-11-02 00:31:26 -0500

    adds created_on to Image model

--- main_app/models.py
+++ main_app/models.py
@@ -56,6 +56,10 @@ class Comment(models.Model):
 class Image(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
 
 
 
