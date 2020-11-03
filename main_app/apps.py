ddc6ae07ad189689e2f28e7af5207fee7bbd0085
BTB0923 <brendan.t.bliss@gmail.com>
2020-11-03 02:14:09 -0500

    adds postman framework, adds navbar styling

--- main_app/apps.py
+++ main_app/apps.py
@@ -1,5 +1,34 @@
 from django.apps import AppConfig
+from django.conf import settings #delete this line
+from django.db.models.signals import post_migrate #delete this line
+from django.utils.translation import gettext_noop as _ #delete this line
 
 
 class MainAppConfig(AppConfig):
     name = 'main_app'
+
+
+#DELETE EVERYTHING BELOW THIS LINE
+
+notifier_app_label = getattr(settings, 'POSTMAN_NOTIFIER_APP', 'pinax_notifications')
+if notifier_app_label:
+    def create_notice_types(*args, **kwargs):
+        create = apps.get_model(notifier_app_label, 'NoticeType').create
+        create("postman_rejection", _("Message Rejected"), _("Your message has been rejected"))
+        create("postman_message", _("Message Received"), _("You have received a message"))
+        create("postman_reply", _("Reply Received"), _("You have received a reply"))
+
+class PostmanConfig(AppConfig):
+    name = 'postman'
+
+    def ready(self):
+        from .models import setup
+        setup()
+
+        if notifier_app_label:
+            try:
+                notifier_app_config = apps.get_app_config(notifier_app_label)
+            except LookupError:  # means the app is not in INSTALLED_APPS, which is valid
+                pass
+            else:
+                post_migrate.connect(create_notice_types, sender=notifier_app_config)
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path, include
+from django import VERSION #delete this line
+from django.urls import path, include, re_path #delete re_path
 from django.conf.urls import url
 from . import views
-from .views import home, signup, activation_sent_view, activate
+from .views import home, signup, activation_sent_view, activate, InboxView, SentView, ArchivesView, TrashView, WriteView, ReplyView, MessageView, ConversationView, ArchiveView, DeleteView, UndeleteView, MarkReadView, MarkUnreadView, IndexView #delete InboxView, SentView, ArchivesView, TrashView, WriteView, ReplyView, MessageView, ConversationView, ArchiveView, DeleteView, UndeleteView, MarkReadView, MarkUnreadView, IndexView
 from django.conf import settings
 from django.conf.urls.static import static
+if getattr(settings, 'POSTMAN_I18N_URLS', False):  #delete this line
+    from django.utils.translation import pgettext_lazy   #delete this line
+else:   #delete this line
+    def pgettext_lazy(c, m): return m   #delete this line
 
+# app_name = 'postman'
 urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile_detail, name='profile'),
@@ -17,10 +23,32 @@ urlpatterns = [
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
     path('posts/<int:post_id>/delete/', views.post_delete, name='post_delete'),
     path('register/', views.signup, name='signup'),
-    path('sent/', activation_sent_view, name="activation_sent"),
+    path('activationsent/', activation_sent_view, name="activation_sent"),
     path('activate/<slug:uidb64>/<slug:token>/', activate, name='activate'),
     path('upload/', views.image_upload_view, name='image_upload_view'),
     path('tank_images/', views.display_images, name = 'images'),
+    
+    #DELETE ALL VIEWS BELOW HERE
+    
+    # Translators: keep consistency of the <option> parameter with the translation for 'm'
+    re_path(pgettext_lazy('postman_url', r'^inbox/(?:(?P<option>m)/)?$'), InboxView.as_view(), name='inbox'),
+    # Translators: keep consistency of the <option> parameter with the translation for 'm'
+    re_path(pgettext_lazy('postman_url', r'^sent/(?:(?P<option>m)/)?$'), SentView.as_view(), name='sent'),
+    # Translators: keep consistency of the <option> parameter with the translation for 'm'
+    re_path(pgettext_lazy('postman_url', r'^archives/(?:(?P<option>m)/)?$'), ArchivesView.as_view(), name='archives'),
+    # Translators: keep consistency of the <option> parameter with the translation for 'm'
+    re_path(pgettext_lazy('postman_url', r'^trash/(?:(?P<option>m)/)?$'), TrashView.as_view(), name='trash'),
+    re_path(pgettext_lazy('postman_url', r'^write/(?:(?P<recipients>[^/#]+)/)?$'), WriteView.as_view(), name='write'),
+    re_path(pgettext_lazy('postman_url', r'^reply/(?P<message_id>[\d]+)/$'), ReplyView.as_view(), name='reply'),
+    re_path(pgettext_lazy('postman_url', r'^view/(?P<message_id>[\d]+)/$'), MessageView.as_view(), name='view'),
+    # Translators: 't' stands for 'thread'
+    re_path(pgettext_lazy('postman_url', r'^view/t/(?P<thread_id>[\d]+)/$'), ConversationView.as_view(), name='view_conversation'),
+    re_path(pgettext_lazy('postman_url', r'^archive/$'), ArchiveView.as_view(), name='archive'),
+    re_path(pgettext_lazy('postman_url', r'^delete/$'), DeleteView.as_view(), name='delete'),
+    re_path(pgettext_lazy('postman_url', r'^undelete/$'), UndeleteView.as_view(), name='undelete'),
+    re_path(pgettext_lazy('postman_url', r'^mark-read/$'), MarkReadView.as_view(), name='mark-read'),
+    re_path(pgettext_lazy('postman_url', r'^mark-unread/$'), MarkUnreadView.as_view(), name='mark-unread'),
+    re_path(pgettext_lazy('postman_url', r'^postmanindex/$'), IndexView.as_view(), name='index'),
 ]
 
 if settings.DEBUG:
